ced7c3b4bc358bd28c2932721c4899dbab26c362
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-07-03 18:23:30 -0300

    Projeto finalizado (Retirado os comentarios)

--- DesvendandoDapper/Program.cs
+++ DesvendandoDapper/Program.cs
@@ -10,33 +10,33 @@ using (var connection = new SqlConnection(connectionString))
     connection.Open();
 
     //Retorna a quantidade de linhas afetadas
-    /*
+    
     var sqlInsert = "INSERT INTO Orders(OrderId, CustomersFk,OrderDetails) VALUES(@OrderId, @CustomersFk, @OrderDetails)";
     connection.Execute(sqlInsert, new { OrderId = 1, CustomersFk = 1, OrderDetails = "Detalhes" });
-    */
+    
 
     //Retorna o Id INSERIDO
-    /*
+    
     var sqlInsert2 = "INSERT INTO Orders OUTPUT INSERTED.OrderId VALUES(@OrderId, @CustomersFk, @OrderDetails)";
     var id = connection.ExecuteScalar<int>(sqlInsert2, new { OrderId = 2, CustomersFk = 1, OrderDetails = "Detalhes" });
-    */
+    
 
     //UPDATE
-    /*
+    
     var sqlUpdate = "Update Orders Set OrderDetails = @OrderDetails WHERE OrderId = @OrderId";
     connection.Execute(sqlUpdate, new { OrderDetails = "Update Details", OrderId = 1 });
-    */
+    
 
     //Stored Procedures
-    /*
+    
     var parameters = new DynamicParameters();
     parameters.Add("@OrderId", 1);
 
     var orderDetailsFromSp = connection.Query<OrderDetailResult>("GetOrderDetails", parameters, commandType: System.Data.CommandType.StoredProcedure).SingleOrDefault();
-    */
+    
 
     //Select SIMPLES
-    /*
+    
     var sqlOrderById = @"SELECT * FROM Orders Where OrderId = @OrderId";
     var sqlOrderByIdResult = connection.QuerySingleOrDefault<Order>(sqlOrderById, new { OrderId = 2 });
 
@@ -52,7 +52,7 @@ using (var connection = new SqlConnection(connectionString))
 	    WHERE o.OrderId = @OrderId;";
 
     var sqlOrderByIdProjectionResult = connection.Query<OrderDetailResult>(sqlOrderByIdProjection, new { OrderId = 1}).SingleOrDefault();
-    */
+    
 
     //SELECT com objetos mesclados
     var sqlOrderByIdProjection = @"SELECT
