7d858603c4ad78c671cb2e895b1379688d4b8e35
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-07-03 15:11:39 -0300

    Criado as classes e adicionado as bibliotecas necessarias

---
+++ DesvendandoDapper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+var connectionString = "Server=localhost; Database=DotLive08_Ecommerce; User Id=sa; Password=1q2w3e4r@#$;trustServerCertificate=true;MultipleActiveResultSets=True";
+
+
+using (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+}
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public int CustomersFk { get; set; }
+    public string OrderDetails { get; set; }
+    public Customer Customer { get; set; }
+    public List<OrderItem> Items { get; set; }
+}
+
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
+}
+
+public class OrderItem
+{
+    public int OrderItemId { get; set; }
+    public string ProductName { get; set; }
+    public int ProductQuantity { get; set; }
+    public int OrderFk { get; set; }
+}
